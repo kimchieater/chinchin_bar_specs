960cf1dbbd102f1339e4a3792b582a5bb58daa25
kimchieater <jasonj19276@gmail.com>
2024-02-13 22:12:41 +1100

    created specs page

---
+++ chinchin_specs/app/quiz/page.js
@@ -0,0 +1,11 @@
+
+
+export default function Quiz(){
+  return(
+    <div className="quiz">
+    
+    <h1>Hello</h1>
+      
+    </div>
+  )
+}
--- chinchin_specs/app/specs/page.js
+++ chinchin_specs/app/specs/page.js
@@ -2,8 +2,23 @@
 
 export default function Specs(){
   return(
-    <div>
-      <h1>Hello</h1>
+    <div className="specs">
+      <div className="search">
+        <input className="search-input" name="search-input" type="text"></input>
+        <button className="search-button">Search</button>
+      </div>
+      <div className="cocktail-info">
+        <h2>Search Cocktails</h2>
+          <div className="cocktail-info-container">
+            <img src="/cocktail-img.png"></img>
+            <div className="cocktail-info-section">
+              <h3>Old Fashioned</h3>
+              <p>Cocktail specs</p>
+              <p>Cocktail origin</p>
+            </div>
+          </div>
+        
+      </div>
     </div>
   )
 }

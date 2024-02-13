d86e36e2f7c2c6846b7081db6f0cf8fc4bc40d0e
kimchieater <jasonj19276@gmail.com>
2024-02-13 22:27:30 +1100

    updated quiz

--- chinchin_specs/app/specs/page.js
+++ chinchin_specs/app/specs/page.js
@@ -13,8 +13,8 @@ export default function Specs(){
             <img src="/cocktail-img.png"></img>
             <div className="cocktail-info-section">
               <h3>Old Fashioned</h3>
-              <p>Cocktail specs</p>
-              <p>Cocktail origin</p>
+              <p>60ml VODKA 30ml HENESSEY 30ml CHAMBORD 60ml SUGAR SYRUP</p>
+              <p>Some say that the drink actually originated in Louisville, Kentucky in 1880 at a private social club called The Pendennis Club. The recipe is linked to bartender and bourbon distillery, James E. Pepper.</p>
             </div>
           </div>
         

32334fa679dd661002bd38cb2328b50cde7d21fd
kimchieater <jasonj19276@gmail.com>
2024-02-26 13:56:28 +1100

    updated

--- chinchin_specs/app/quiz/page.js
+++ chinchin_specs/app/quiz/page.js
@@ -3,9 +3,23 @@
 export default function Quiz(){
   return(
     <div className="quiz">
-    
-    <h1>Hello</h1>
-      
+      <div className="quiz-bg">
+        <div className="leaderboard">
+          <h2>Leaderboard</h2>
+          <h4>1. Leo</h4>
+          <h4>2. Spike</h4>
+        </div>
+        <div className="quiz-section">
+          <h3>QUESTION</h3>
+          <div className="quiz-section-options">
+          <p>1 awdawdawdadwadawdawdawd </p>
+          <p>2 awdawdawdawdawdawdawdawd</p>
+          <p>3 awdawdawdawdawdawdawdawda </p>
+          <p>4 awdawdawdawdawdawdawdawdaw</p>
+          </div>
+          
+        </div>
+      </div>
     </div>
   )
 }
